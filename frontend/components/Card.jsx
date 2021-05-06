51f77cfcc33c0c64fd16dd600fc08fb47e0d6f5e
Jae Park <jaeepark@gmail.com>
2021-05-06 14:16:23 -0700

    Card component + css complete

--- frontend/components/Card.jsx
+++ frontend/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import dalme from '../static/mentor_seodalme.png'; // TODO: Get image source from backend
+import dalme from '../static/dalme.png'; // TODO: Get image source from backend
+import favIcon from '../static/favorite_icon.png';
 
 class Card extends React.Component {
   constructor(props) {
@@ -23,11 +24,21 @@ class Card extends React.Component {
     let mentor = this.state.mentor
 
     return (
-              <div className="card-wrapper">
-                  <a href="/mentor">
-                      <img className="card-image" src={mentor.imageSource} />
+              <div className="card-container">
+                  <a className="card-container" href="/mentor">
+                      <h4 className="card-header">
+                        좋은 기술로 일상의 문제 해결하기
+                      </h4>
+                      <div className="card-image-container">
+                        <img className="fav-icon" src={favIcon} />
+                        <div className="card-frame-circle">
+                          <img className="card-image" src={mentor.imageSource} />
+                        </div>
+                      </div>
                       <div className="card-description">
-                        DALME
+                        <p className="mentor-name">서달미</p>
+                        <p className="mentor-position">삼산텍 CEO</p>
+                        <p className="mentor-interests">채용, 투자유치, IR 등</p>
                       </div>
                   </a>
               </div>
