30ef1ba095289ea899f6f0aa66dbc04c585d6e10
KweonMo Koo <mikekoo2010@gmail.com>
2021-05-05 19:31:16 -0700

    Create forms

---
+++ frontend/components/InterestForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InterestForm = () => {
+
+
+  return (
+    <></>
+  )
+}
+
+export default InterestForm;
---
+++ frontend/components/PopUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PopUp = () => (
+  <div className="popup-wrapper">
+    
+  </div>
+)
+
+export default PopUp;
