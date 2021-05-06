197c7b79a830e2fdc3510a694ce011b5df6f39e1
KweonMo Koo <mikekoo2010@gmail.com>
2021-05-05 22:11:37 -0700

    Add context provider

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,13 +6,18 @@ import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import Calendar from './pages/Calendar';
 import Reviews from './pages/Reviews';
+import { SignUpContext } from './SignUpContext'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route exact path='/sign-up' component={SignUp} />
+        <Route path='/sign-up'>
+          <SignUpContext.Provider>
+            <SignUp />
+          </SignUpContext.Provider>
+        </Route>
         <Route exact path='/calendar' component={Calendar} />
         <Route exact path='/reviews' component={Reviews} />
         <Route component={PageNotFound} />
