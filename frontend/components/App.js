66aa9c1b79da6cf5c1674bfd0774666bd12f0437
KweonMo Koo <mikekoo2010@gmail.com>
2021-05-06 18:57:50 -0700

    Map new url

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,20 +4,20 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import PageNotFound from './pages/PageNotFound';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
+import RegisterSchool from './pages/RegisterSchool';
+import MyInterest from './pages/MenteeProfile';
 import Calendar from './pages/Calendar';
 import Reviews from './pages/Reviews';
-import { SignUpContext } from './SignUpContext'
+// import { SignUpContext } from './SignUpContext'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route path='/sign-up'>
-          <SignUpContext.Provider>
-            <SignUp />
-          </SignUpContext.Provider>
-        </Route>
+        <Route exact path='/sign-up' component={SignUp} />
+        <Route exact path='/sign-up/basic-info' component={RegisterSchool} />
+        <Route exact path='/profile/mentee' component={MenteeProfile} />
         <Route exact path='/calendar' component={Calendar} />
         <Route exact path='/reviews' component={Reviews} />
         <Route component={PageNotFound} />
