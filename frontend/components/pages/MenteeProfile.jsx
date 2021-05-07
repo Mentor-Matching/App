1d522dc2e663660f02c8e54678ebb9d532ff917a
KweonMo Koo <mikekoo2010@gmail.com>
2021-05-06 18:58:17 -0700

    Create mentee profile page

---
+++ frontend/components/pages/MenteeProfile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MenteeProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+
+    return <div>Mentee Profile page</div>
+  }
+}
+
+export default MenteeProfile;
