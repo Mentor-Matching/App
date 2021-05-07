d93d11bcb33c605bf03b4367d5d8be47131fd932
Jae Park <jaeepark@gmail.com>
2021-05-06 22:46:18 -0700

    Complete landing page banner and search box

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -4,9 +4,11 @@ import Footer from './Footer'
 
 const Layout = ({children}) => (
   <>
-    <Header />
-      <main>{children}</main>
-    <Footer />
+    <div className="main-container">
+      <Header />
+        <main>{children}</main>
+      <Footer />
+    </div>
   </>
 )
 
--- frontend/components/pages/Home.jsx
+++ frontend/components/pages/Home.jsx
@@ -28,50 +28,46 @@ class Home extends React.Component {
   render() {
     return (
       <Layout>
-        <h1>Home page</h1>
         <div className="main-content-container">
 
-          {/* Banner Section */}
           <div className="inner-container">
+            {/* Banner Section */}
             <div className="banner-container">
-              <div className="banner-rectangle-2" />
-              <div className="banner-rectangle-2" />
               <div className="banner-rectangle-1">
-                <div className="banner-rectangle-1">
-                  <div className="banner-content-wrapper">
-                    <h2 className="banner-title">
-                      학생 주도 멘토 연결 웹 플랫폼
-                    </h2>
-                    <p className="banner-text">
-                      "인간의 삶에서 가장 거대한 변수는 주변 사람, 주변 사람이 무엇을 하고 무엇을 하지 않느냐가 우리 행동에 결정적인 영향을 끼친다"
-                    </p>
-                    <p className="banner-text">
-                      - 쿠르트 레빈, 심리학자
-                    </p>
-                  </div>
-                </div>
               </div>
-
-
-              {/* Search Box Section */}
-              <div className="searchbox-container">
-                <div className="searchbox">
-                  <div className="search-input">
-                  </div>
-                  <button className="search-button">
-                    멘토 찾기
-                  </button>
+              <div className="banner-rectangle-2">
+                <div className="banner-content-wrapper">
+                  <h3 className="banner-title">
+                    학생 주도 멘토 연결 웹 플랫폼
+                  </h3>
+                  <p className="banner-text">
+                    "인간의 삶에서 가장 거대한 변수는 주변 사람, 주변 사람이 무엇을 하고 무엇을 하지 않느냐가 우리 행동에 결정적인 영향을 끼친다"
+                  </p>
+                  <p className="banner-text">
+                    - 쿠르트 레빈, 심리학자
+                  </p>
                 </div>
               </div>
             </div>
-
-            <div className="inner-container">
-              <div className="card-list-container">
-                <Card />
+            {/* Search Box Section */}
+            <div className="searchbox-container">
+              <div className="search-box">
+                <input className="search-input" placeholder="멘토, 직업 또는 회사로 검색어를 입력하세요.">
+                </input>
+                <button className="search-button">
+                  멘토 찾기
+                </button>
               </div>
             </div>
-
           </div>
+
+          {/* Card List Section */}
+          <div className="inner-container">
+            <div className="card-list-container">
+              <Card />
+            </div>
+          </div>
+
         </div>
         {/* Banner */}
         {/* Search */}
