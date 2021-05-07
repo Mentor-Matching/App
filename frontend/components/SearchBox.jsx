b5866ad0a72871863e66b9d929d49c2a01420f96
Jae Park <jaeepark@gmail.com>
2021-05-06 17:37:28 -0700

    Checkpoint

---
+++ frontend/components/SearchBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class SearchBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mentors: []
+    }
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
+}
+
+export default SearchBox;
