8bfe5a321e86dcdb9b3c18d03addc40185135dfc
KweonMo Koo <mikekoo2010@gmail.com>
2021-05-08 01:05:03 -0700

    Update double rectangle design

--- frontend/components/pages/Home.jsx
+++ frontend/components/pages/Home.jsx
@@ -75,20 +75,22 @@ class Home extends React.Component {
           <div className="inner-container">
             {/* Banner Section */}
             <div className="banner-container">
-              <div className="banner-rectangle-1">
-              </div>
-              <div className="banner-rectangle-2">
-                <div className="banner-content-wrapper">
-                  <h3 className="banner-title">
-                    학생 주도 멘토 연결 웹 플랫폼
-                  </h3>
-                  <p className="banner-text">
-                    "인간의 삶에서 가장 거대한 변수는 주변 사람, 주변 사람이 무엇을 하고 무엇을 하지 않느냐가 우리 행동에 결정적인 영향을 끼친다"
-                  </p>
-                  <p className="banner-text">
-                    - 쿠르트 레빈, 심리학자
-                  </p>
+              <div className="banner-rectangle-wrapper">
+                <div className="banner-rectangle--top">
                 </div>
+                <div className="banner-rectangle--bottom">
+                </div>
+              </div>
+              <div className="banner-content-wrapper">
+                <h3 className="banner-title">
+                  학생 주도 멘토 연결 웹 플랫폼
+                </h3>
+                <p className="banner-text">
+                  "인간의 삶에서 가장 거대한 변수는 주변 사람, 주변 사람이 무엇을 하고 무엇을 하지 않느냐가 우리 행동에 결정적인 영향을 끼친다"
+                </p>
+                <p className="banner-text">
+                  - 쿠르트 레빈, 심리학자
+                </p>
               </div>
             </div>
             {/* Search Box Section */}
